6784561f86daa71d2a318434ce279747464e1594
Josiane Reis <tqi_jreis@uolinc.com>
2020-11-17 18:21:45 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <main>
+        <h1>hello next</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
